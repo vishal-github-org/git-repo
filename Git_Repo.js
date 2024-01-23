7edc9de1786165cb3057379698bf7fd76ce94155
Vishal Khape <127313030+vishal-github-org@users.noreply.github.com>
2024-01-23 15:39:31 +0530

    Update Git_Repo.js
    Removed access token

--- Git_Repo.js
+++ Git_Repo.js
@@ -1,34 +1,29 @@
+function getRepositories() {
+    const username = document.getElementById('username').value.trim();
+    const resultContainer = document.getElementById('result');
 
-        function getRepositories() {
-            const username = document.getElementById('username').value.trim();
-            const resultContainer = document.getElementById('result');
-            
-            // Clear previous results
-            resultContainer.innerHTML = '';
+    // Clear previous results
+    resultContainer.innerHTML = '';
 
-            if (username === '') {
-                alert('Please enter a GitHub username.');
-                return;
+    if (username === '') {
+        alert('Please enter a GitHub username.');
+        return;
+    }
+
+    const apiUrl = `https://api.github.com/users/${username}/repos`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.message === 'Not Found') {
+                resultContainer.innerHTML = `<p>User not found.</p>`;
+            } else {
+                const repositories = Array.isArray(data) ? data.map(repo => `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`).join('') : '<p>No repositories found.</p>';
+                resultContainer.innerHTML = `<ul>${repositories}</ul>`;
             }
-
-           
-		// my access token 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
-            const accessToken = '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';
-            const apiUrl = `https://api.github.com/users/${username}/repos?access_token=${accessToken}`;
-
-            fetch(apiUrl, { headers: { Authorization: `Bearer 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`}})
-                .then(response => response.json())
-                .then(data => {
-                    if (data.message === 'Not Found') {
-                        resultContainer.innerHTML = `<p>User not found.</p>`;
-                    } else {
-                       
-                        const repositories = Array.isArray(data)? data.map(repo => `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`).join(''): '<p>No repositories found.</p>';
-                        resultContainer.innerHTML = `<ul>${repositories}</ul>`;
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                    resultContainer.innerHTML = `<p>An error occurred while fetching data.</p>`;
-                });
-        }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            resultContainer.innerHTML = `<p>An error occurred while fetching data.</p>`;
+        });
+}
